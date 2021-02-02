41bb2a27453431be89d309dd606623ffa09c9f07
chas-e <sigmapi76388@gmail.com>
2021-02-02 18:32:08 -0500

    adds templates

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,20 @@
+from flask import render_template
+
 from app import app
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return 'Hello, World!'
+    user = {'username': 'Chas'}
+    posts = [
+        {
+            'author': {'username': 'John'},
+            'body': 'A Beautiful Day in Portland!'
+        },
+        {
+            'author': {'username': 'Jane'},
+            'body': 'That new Amazon show "Flack" is da bomb!'
+        },
+    ]
+    return render_template('index.html', title="Home", user=user, posts=posts)
 
